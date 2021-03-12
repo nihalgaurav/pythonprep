279ab59710a5cd684d71a8a68a03511a65a0629f
NihalGaurav <nihalgaurav85@gmail.com>
2021-03-12 08:29:25 +0530

    square star pattern pro :)

--- patternPrint.py
+++ patternPrint.py
@@ -12,3 +12,15 @@ for i in range(0,N//2 +1):
     print(("*"*(2*i+1)).center(N, " "))
 
 
+n = N
+
+for i in range(n):
+    print(("*"*(2*i+1)).center(2*n-1, " "))
+for i in range(n):
+    i = n-i-1
+    print(("*"*(i+1)).rjust(n, " "))
+for i in range(n):
+    print(("*"*(i+1)).rjust(n, " "))
+for i in range(n):
+    i = n-i -1
+    print(("*"*(2*i+1)).center(2*n-1, " "))
